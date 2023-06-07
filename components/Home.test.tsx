e101055fd6a3028d18a81e34eeec93b87e5fe169
Andrew Vo-Nguyen <andrew.vo89@gmail.com>
2023-06-07 10:23:30 +1000

    Add unit tests for Home

--- components/Home.test.tsx
+++ components/Home.test.tsx
@@ -1,23 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { expect, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import AppWrapper from 'components/AppWrapper';
+import { Destination } from 'types/common';
 import Home from 'components/Home';
-import { expect } from 'vitest';
+import { act } from 'react-dom/test-utils';
+import { faker } from '@faker-js/faker';
+import { useHistoryStore } from 'store/history';
+import { useNodeStore } from 'store/node';
 
 test('renders main container', () => {
-  render(
-    <AppWrapper>
-      <Home prompts={[]} initialEdges={[]} initialJNodes={[]} initialNodes={[]} placeholder='' />
-    </AppWrapper>,
-  );
+  render(<Home prompts={[]} initialEdges={[]} initialJNodes={[]} initialNodes={[]} placeholder='' />, {
+    wrapper: AppWrapper,
+  });
   expect(screen.getByRole('main', { name: 'Main container' })).toBeDefined();
 });
 
 test('renders right container', () => {
-  render(
-    <AppWrapper>
-      <Home prompts={[]} initialEdges={[]} initialJNodes={[]} initialNodes={[]} placeholder='' />
-    </AppWrapper>,
-  );
+  render(<Home prompts={[]} initialEdges={[]} initialJNodes={[]} initialNodes={[]} placeholder='' />, {
+    wrapper: AppWrapper,
+  });
   expect(screen.getByRole('region', { name: 'Right container' })).toBeDefined();
+});
+
+test('updates nodes with no destinations', () => {
+  const mockUpdateNodes = vi.fn();
+  useNodeStore.getState().updateNodes = mockUpdateNodes;
+
+  render(<Home prompts={[]} initialEdges={[]} initialJNodes={[]} initialNodes={[]} placeholder='' />, {
+    wrapper: AppWrapper,
+  });
+  expect(mockUpdateNodes).toHaveBeenCalledWith([]);
+});
+
+test('updates nodes with selected destinations', async () => {
+  const prompt = faker.lorem.sentence();
+  const destinations: Destination[] = [{ id: faker.string.uuid(), enabled: true }];
+
+  const mockUpdateNodes = vi.fn();
+  useNodeStore.getState().updateNodes = mockUpdateNodes;
+
+  act(() => {
+    useHistoryStore.getState().setSelected({
+      createdAt: faker.date.past().toISOString(),
+      destinations,
+      id: faker.string.uuid(),
+      prompt: { label: prompt, value: prompt },
+    });
+  });
+  render(<Home prompts={[]} initialEdges={[]} initialJNodes={[]} initialNodes={[]} placeholder='' />, {
+    wrapper: AppWrapper,
+  });
+  expect(mockUpdateNodes).toHaveBeenCalledWith(destinations);
 });
